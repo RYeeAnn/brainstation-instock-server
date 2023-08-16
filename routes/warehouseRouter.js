7aa89fb74c6615ee107d63b4a05338427abf2d81
supreetkaur <supreet97@gmail.com>
2023-08-16 12:04:05 -0700

    mock route for getting single warehouse

--- routes/warehouseRouter.js
+++ routes/warehouseRouter.js
@@ -5,11 +5,23 @@ router
   .route("/")
   .get((req, res) => {
     console.log("Get on warehouse");
+    knex("user");
     res.send("Get on warehouse");
   })
   .post((req, res) => {
     console.log("Post on warehouse");
     res.send("Post on warehouse");
+  });
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log("Get single warehouse");
+    res.send("Get single warehouse");
+  })
+  .post((req, res) => {
+    console.log("Post single warehouse");
+    res.send("Post single warehouse");
   });
 
 module.exports = router;
