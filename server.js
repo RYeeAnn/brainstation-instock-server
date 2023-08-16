9ca78eabcfeed35b7c1f734d004bcea1aa8e2a52
supreetkaur <supreet97@gmail.com>
2023-08-16 11:34:41 -0700

    migrations and seeds

--- server.js
+++ server.js
@@ -8,20 +8,16 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5050;
 
-
 app.use(cors());
 app.use(express.json());
 app.use("/warehouse", warehouseRoute);
 app.use("/inventory", inventoryRoute);
 
-
-
 app.get("/", (req, res) => {
-    console.log("Made it to the server!!");
-    res.send("Welcome to the server!!");
+  console.log("Made it to the server!!");
+  res.send("Welcome to the server!!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${process.env.PORT}`);
-})
-
+  console.log(`Listening on ${process.env.PORT}`);
+});
