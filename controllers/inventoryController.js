2f674095204b9c8433eff402c33ae999d34b4c86
supreetkaur <supreet97@gmail.com>
2023-08-18 12:41:24 -0700

    join to get warehouse name for all inventories

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,54 +1,48 @@
 const knex = require("knex")(require("../knexfile.js"));
 
 const getAllInventory = (req, res) => {
-    knex("inventories")
-      .then((data) => {
-        res.status(200).json(data);
-      })
-      .catch((err) =>
-        res.status(400).send(`Error retrieving Inventory: ${err}`)
-      );
-  };
+  knex("inventories")
+    .join("warehouses", "warehouse_id", "warehouses.id")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Inventory: ${err}`));
+};
 
+const getSingleInventory = (req, res) => {
+  const inventoryId = req.params.id;
 
+  knex("inventories")
+    .where("id", inventoryId)
+    .first()
+    .then((inventory) => {
+      if (!inventory) {
+        return res
+          .status(404)
+          .json({ message: `Inventory with ID ${inventoryId} not found` });
+      }
+      res.status(200).json(inventory);
+    })
+    .catch((err) =>
+      res.status(400).json({ error: `Error retrieving Inventory: ${err}` })
+    );
+};
 
+const getInventoriesForWarehouse = (req, res) => {
+  const warehouseId = req.params.warehouse_id;
 
-  const getSingleInventory = (req, res) => {
-    const inventoryId = req.params.id;
-  
-    knex("inventories")
-      .where("id", inventoryId)
-      .first()
-      .then((inventory) => {
-        if (!inventory) {
-          return res.status(404).json({ message: `Inventory with ID ${inventoryId} not found` });
-        }
-        res.status(200).json(inventory);
-      })
-      .catch((err) => res.status(400).json({ error: `Error retrieving Inventory: ${err}` }));
-  };
+  knex("inventories")
+    .where({ warehouse_id: warehouseId })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) =>
+      res.status(400).send(`Error retrieving Inventories for Warehouse: ${err}`)
+    );
+};
 
-
-
-
-  const getInventoriesForWarehouse = (req, res) => {
-    const warehouseId = req.params.warehouse_id;
-  
-    knex("inventories")
-      .where({ warehouse_id: warehouseId })
-      .then((data) => {
-        res.status(200).json(data);
-      })
-      .catch((err) =>
-        res.status(400).send(`Error retrieving Inventories for Warehouse: ${err}`)
-      );
-  };
-  
-  
-  module.exports = {
-    getAllInventory,
-    getSingleInventory,
-    getInventoriesForWarehouse
-    
-  };
-  
+module.exports = {
+  getAllInventory,
+  getSingleInventory,
+  getInventoriesForWarehouse,
+};
